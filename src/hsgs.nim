adf508e571180ee7632a7dbbf6700280ccadc79c
Nycto <James@RoundEights.com>
2025-03-18 18:02:35 -0700

    Initial implementation skeleton

--- src/hsgs.nim
+++ src/hsgs.nim
@@ -1,7 +1,97 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+##
+## Hierarchical Spatial Hash Grid
+##
 
-proc add*(x, y: int): int =
-  ## Adds two numbers together.
-  return x + y
+import std/[tables, sets]
+
+type
+  CellKey = tuple[x, y: int32]
+
+  SpatialObject* = concept obj
+    ## A value that can be stored in a 2d HSGS
+    obj.x is int32
+    obj.y is int32
+    obj.width is int32
+    obj.height is int32
+
+  HSGS*[T: SpatialObject] = ref object
+    ## A hierarchical spatial hash grid
+    next: HSGS[T]
+    cellSize: int32
+    cells: Table[CellKey, seq[T]]
+
+proc newLayer[T](cellSize: int32): auto =
+  HSGS[T](cellSize: cellSize, cells: initTable[CellKey, seq[T]]())
+
+proc newHSGS*[T](numLevels: SomeInteger, baseCellSize: int32): HSGS[T] =
+  ## Create a new HSGS store
+  result = newLayer[T](baseCellSize)
+  var current = result
+  for i in 1..<numLevels:
+    current.next = newLayer[T](current.cellSize * 2)
+    current = current.next
+
+proc getCellIndex[T](grid: HSGS[T], x, y: int32): CellKey =
+  (x div grid.cellSize, y div grid.cellSize)
+
+iterator eachLayer[T](grid: HSGS[T], grow: bool = false): HSGS[T] =
+  var layer = grid
+  while layer != nil:
+    yield layer
+    if grow and layer.next == nil:
+      layer.next = newLayer[T](layer.cellSize * 2)
+    layer = layer.next
+
+proc `$`*(grid: HSGS): string =
+  result = "HSGS:\n"
+  for layer in grid.eachLayer():
+    result &= "  Level: " & $layer.cellSize & "\n"
+    for key, cell in layer.cells.pairs:
+      result &= "    Cell: " & $key & "\n"
+      for obj in cell:
+        result &= "     " & $obj & "\n"
+
+iterator cellRange(minCell, maxCell: CellKey): CellKey =
+  for x in minCell.x..maxCell.x:
+    for y in minCell.x..maxCell.y:
+      yield (x, y)
+
+proc add*[T](grid: HSGS[T], obj: T) =
+  ## Add a value to this spacial grid
+  let size = max(obj.width, obj.height)
+
+  for layer in grid.eachLayer(grow = true):
+    if layer.cellSize >= size:
+      let minCell = layer.getCellIndex(obj.x, obj.y)
+      let maxCell = layer.getCellIndex(obj.x + obj.width, obj.y + obj.height)
+      for cell in cellRange(minCell, maxCell):
+        layer.cells.mgetOrPut(cell, @[]).add(obj)
+      return
+
+  raiseAssert("Could not find a layer to add to")
+
+iterator find*[T](grid: HSGS[T]; x, y, radius: int32): T =
+  ## Finds all the values within a given radius of a point
+  var seen = initHashSet[T]()  # Track yielded objects
+  for layer in grid.eachLayer():
+    let minCell = layer.getCellIndex(x - radius, y - radius)
+    let maxCell = layer.getCellIndex(x + radius, y + radius)
+    for cell in cellRange(minCell, maxCell):
+      if cell in layer.cells:
+        for obj in layer.cells[cell]:
+          if obj notin seen:
+            seen.incl(obj)
+            yield obj
+
+# proc remove*[T, O](grid: HSGS[T, O], obj: O) =
+#   ## Remove a value from this layer
+#   for layer in grid.eachLayer():
+#     let minCell = layer.getCellIndex(obj.x, obj.y)
+#     let maxCell = layer.getCellIndex(obj.x + obj.width, obj.y + obj.height)
+#     for cell in cellRange(minCell[0], minCell[1], maxCell[0], maxCell[1]):
+#       if cell in layer.cells:
+#         layer.cells[cell].keepItIf(proc(o: O): bool = o != obj)
+# 
+# proc clear[T, O](grid: HSGS[T, O]) =
+#   for layer in grid.eachLayer():
+#     layer.cells.clear()
---
+++ tests/t_hsgs.nim
@@ -0,0 +1,78 @@
+import std/[unittest, math, sequtils], hsgs
+
+type
+  GameObject = object
+    name*: string
+    x*, y*, width*, height*: int32
+
+proc obj(name: string; x, y, width, height: int32): auto =
+  GameObject(name: name, x: x, y: y, width: width, height: height)
+
+suite "Hierarchical Spatial Hash Grid Tests":
+
+  let rock = obj("Rock", x = 5, y = 5, width = 3, height = 3)
+  let tree = obj("Tree", x = 20, y = 20, width = 15, height = 15)
+
+  test "Insertion and Query":
+    var grid = newHSGS[GameObject](3, 2)
+
+    grid.add(rock)
+    grid.add(tree)
+
+    echo $grid
+
+    check(grid.find(5, 5, 2).toSeq.mapIt(it.name) == @[ "Rock" ])
+
+    # check found, "Rock should be found within query range"
+
+#   test "Removal":
+#     let bush = GameObject(name: "Bush", pos: (10.0, 10.0), size: (2.0, 2.0))
+#     grid.insert(bush)
+# 
+#     check len(toSeq(grid.query((10.0, 10.0), radius = 5.0))) == 1
+#     grid.remove(bush)
+#     check len(toSeq(grid.query((10.0, 10.0), radius = 5.0))) == 0, "Bush should be removed"
+# 
+#   test "Clear":
+#     let rock = GameObject(name: "Rock", pos: (5.0, 5.0), size: (3.0, 3.0))
+#     let tree = GameObject(name: "Tree", pos: (20.0, 20.0), size: (15.0, 15.0))
+#     grid.insert(rock)
+#     grid.insert(tree)
+# 
+#     grid.clear()
+#     check len(toSeq(grid.query((0.0, 0.0), radius = 50.0))) == 0, "Grid should be empty after clear"
+# 
+#   test "Query Multiple Levels":
+#     let largeObject = GameObject(name: "LargeObject", pos: (30.0, 30.0), size: (20.0, 20.0))
+#     let smallObject = GameObject(name: "SmallObject", pos: (32.0, 32.0), size: (4.0, 4.0))
+# 
+#     grid.insert(largeObject)
+#     grid.insert(smallObject)
+# 
+#     var foundLarge = false
+#     var foundSmall = false
+#     for obj in grid.query((30.0, 30.0), radius = 5.0):
+#       if obj.name == "LargeObject": foundLarge = true
+#       if obj.name == "SmallObject": foundSmall = true
+# 
+#     check foundLarge, "Large object should be found in coarse level"
+#     check foundSmall, "Small object should be found in fine level"
+# 
+#   test "Iterator for Linked Levels":
+#     var count = 0
+#     for _ in grid.traverseNodes():
+#       inc count
+#     check count == 3, "There should be 3 levels in the linked list"
+# 
+#   test "Cell Range Iterator":
+#     let obj1 = GameObject(name: "Obj1", pos: (10.0, 10.0), size: (2.0, 2.0))
+#     let obj2 = GameObject(name: "Obj2", pos: (15.0, 15.0), size: (2.0, 2.0))
+#     
+#     grid.insert(obj1)
+#     grid.insert(obj2)
+# 
+#     var count = 0
+#     for cell in cellRange((1,1), (2,2)): # Assuming cellSize = 10.0
+#       count += 1
+# 
+#     check count > 0, "Cell range iterator should yield at least one value"
